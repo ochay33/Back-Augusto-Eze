2a93624603f061e4a959df14f08e16119f2594aa
EzeMoreira <eezequielmoreira@gmail.com>
2023-10-13 17:43:16 -0300

    updates finnaly

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken")
 
-const bcrypt = require("bcrypt")
+const bcrypt= require("bcrypt") 
 
 const uuid = require("uuid")
 
-const UserModel = require("../models/User")
+const UserModel = require ("../models/User")
 
-function login(req, res) {
+function login(req,res) {
     const {email, password} = req.body
 
-    UserModel.findOne({email}).then(async response => {
-        if(response?.id) {
+    UserModel.findOne({email}).then(async response =>{
+        if(response?.id){
             const isMatch = await bcrypt.compare(
                 password,
                 response.password
@@ -23,58 +23,58 @@ function login(req, res) {
                             id:response.id,
                         },
                     },
-                    procces.env.SECRET,
+                    process.env.SECRET,
                     {
                         expiresIn:"5h"
                     },
                     (err,token)=>{
                         if(err)res.sendStatus(403)
                         else{
-                    res.status(200).json({
-                        token,
-                        user:{
-                            email:response.email,
-                            id:response.id,
-                            name:response.name,
-                            role:response.role
-                        },
-                    })
+                            res.status(200).json({
+                                token,
+                                user:{
+                                    email:response.email,
+                                    id:response.id,
+                                    name:response.name,
+                                    role:response.role
+                                },
+                            })
+                        }
                     }
+                )
+            }else{
+                res.status(401).json({
+                    message: "Invalid credentials",
+                })
             }
-        )
-}   else{
-    res.status(401).json({
-        message: "Invalid credentials",
-    })
-}
-        } else {
+        }else{
             res.status(401).json({
-                message: "Invalid credentials"
+                message:"Invalid credentials"
             })
         }
     })
 }
 
-async function register (req, res) {
-    try {
-        const {username, password, email, address} = req.bdoy
+async function register  (req,res) {
+    try{
+        const {username,password,email,address} =req.body 
 
-        const salt = bcrypt.genSaltSync(10)
-        const hash = await bcrypt.hash(password, salt)
+        const salt = bcrypt.genSaltSync(10) 
+        const hash = await bcrypt.hash(password,salt)
 
         const data = new UserModel({
-            id: uuid.v4(),
+            id : uuid.v4(),
             username,
-            password: hash,
+            password : hash,
             email,
             address,
-            role: "client",
+            role : "client",
         })
         data.save()
         res.status(201).json({create : true})
-    }   catch(error){
+    }catch(error){
         res.status(400).json({
-            message: error.message
+            message : error.message
         })
     }
 }
--- models/User.js
+++ models/User.js
@@ -1,41 +1,31 @@
 const mongoose = require("mongoose")
+
 const { model, Schema } = mongoose
 
-// creamos la estructura de un tipo de documento (users) 
 const userSchema = new Schema({
-    id: String,
-    name: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        enum: ["admin", "guest"],
-        required: true,
-    },
-     lastName: {
-         type: String,
-         required: true,
-     },
-     userName: {
-         type: String,
-         required: true,
-     },
-     password: {
-         type: String,
-         required: true,
-     },
-     email: {
-         type: String,
-         required: true,
-     },
-})
+	id: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	address: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		enum: ["admin", "client"],
+		required: true,
+	},
+},{versionKey:false})
 
-// Como primer parametro, va el nombre de la coleccion
 module.exports = model("Users", userSchema)
-
-// linea 5, lo que hacemos es instanciar un nuevo esquema, un formato para el nuevo documento, es decir
-// asignale al userschema un schema 
-
-// linea 20, lo que decimos es que vamos a exportar el modelo de users, con el formato de new schema, 
-// aqui deberia permitirnos hacer un post en thunder client y si todo esta correcto enviarse de manera normal
